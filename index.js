2e1566dea60721ca48b7ea2abb1ca1449b5e8fb6
hazrat <tora59459@gmail.com>
2026-06-24 12:30:12 +0600

    cancel booking api added my project added

--- index.js
+++ index.js
@@ -274,9 +274,9 @@ async function run() {
 }
 run().catch(console.dir);
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+// app.get('/', (req, res) => {
+//     res.send('Hello World!')
+// })
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
